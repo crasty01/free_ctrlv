282f69fa48893a14c644bf0309eb431f3753a02c
Daniel Vondra <daniel.vondra@vdttechnology.com>
2022-06-26 12:24:09 +0200

    creaded input.svelte

--- src/lib/components/input.svelte
+++ src/lib/components/input.svelte
@@ -1,18 +1,39 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
 
-  let input: HTMLInputElement;
+	let input: HTMLInputElement;
 	const dispatch = createEventDispatcher();
 
-  const fileUploaded = (payload: any) => {
-    const files = input.files;
-    dispatch('filesSelected', files);
-  }
+	const fileUploaded = (_payload: any) => {
+		const files = input.files;
+		dispatch('filesSelected', files);
+	};
 </script>
 
 <template>
-  <label for="fileUpload">
-    label
+	<label class="label" for="fileUpload"> 
+    <span>drag your file here</span>
+    <span>or click to open a file explorer</span>
   </label>
-  <input type="file" multiple accept="image/*" name="fileUpload" bind:this={input} on:change={fileUploaded}>
+	<input
+		class="input"
+		type="file"
+		multiple
+		accept="image/*"
+		id="fileUpload"
+		bind:this={input}
+		on:change={fileUploaded}
+	/>
 </template>
+
+<style lang="scss">
+	.input {
+		display: none;
+	}
+	.label {
+		display: flex;
+    padding: 1rem;
+    min-width: 6rem;
+    
+	}
+</style>
